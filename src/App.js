f0408ce6f56cb27a835dfcdf9f42ac6c6473736f
Nawazish Ali <nawazishali872@gmail.com>
2019-08-26 12:29:49 +0500

    sample commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import ToDo from './App Folder/ToDo'
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+         <ToDo/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
